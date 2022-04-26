6d9895611b3a4b9d21034e77dabde819fad8a248
Jesús Mudarra Luján <62428273+jmudy@users.noreply.github.com>
2022-04-26 22:31:53 +0200

    Rama nueva creada

---
+++ datasets/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 21:32:38 2022
+
+@author: mudar
+"""
+
+# Plantilla de Pre Procesado
+
+# Como importar las librerias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
